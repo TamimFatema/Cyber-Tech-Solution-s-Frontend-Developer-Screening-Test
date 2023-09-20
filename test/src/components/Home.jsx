51901bf7b0c6a75bdbd98aaa9a13bf9e40d67c4e
TamimFatema <1910807@iub.edu.bd>
2023-09-21 01:44:21 +0600

    main content

--- test/src/components/Home.jsx
+++ test/src/components/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            My country’s name is Bangladesh. Bangladesh is a very small country huge population. It is a medium-developed country in South Asia. We have a very good and progressing economy. It has an area of 147,570 square kilometers. But it has a population of 164 million. That is huge than the area.
-
-            But it is still a very beautiful and peaceful country. People of Bangladesh are so friendly with each other. This country has a glorious history of independence. We got independence from Pakistan in 1971 after nine mothers’ blood-shedding war. More than 3 million Bangladeshi sacrificed their life in that genocide.
-
-            But still, we were able to get our independence. After independence, the newly born country faced lots of hard times and struggles. But it rises very well. Now Bangladesh is one of the best countries to live in South Asia. There are so many beautiful spots to see in this country.
+        <div className="flex w-full flex-col items-center p-10">
+            <h1 className="mb-5 font-black text-5xl">Main Component</h1>
+            <p className="mb-3">My country’s name is Bangladesh. Bangladesh is a very small country
+                huge population. It is a medium-developed country in South Asia. We have a very good and
+                progressing economy. It has an area of 147,570 square kilometers. But it has a population of
+                164 million. That is huge than the area.</p>
+            <p className="mb-3">But it is still a very beautiful and peaceful country.
+                People of Bangladesh are so friendly with each other. This country has a glorious history of
+                independence. We got independence from Pakistan in 1971 after nine mothers’ blood-shedding war.
+                More than 3 million Bangladeshi sacrificed their life in that genocide.</p>
+            <p className="mb-3">But still, we were able to
+                get our independence. After independence, the newly born country faced lots of hard times and
+                struggles. But it rises very well. Now Bangladesh is one of the best countries to live in South Asia.
+                There are so many beautiful spots to see in this country.</p>
         </div>
     )
 }
