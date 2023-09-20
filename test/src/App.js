344b90d96a5a050ccbfd58ed763d09439de19240
TamimFatema <1910807@iub.edu.bd>
2023-09-20 21:02:55 +0600

    react and tailwind setup

---
+++ test/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './index.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
